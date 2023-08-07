785c30f1b8cdb26db4cdbb92816e1b4ba7687649
Nicolas Quiroga <nicolas_quiroga_90@hotmail.com>
2023-08-07 18:06:57 -0300

    Modulo 6 Unidad 1

--- Proyecto/Cortiana Hermanos SRL/backend/routes/admin/administrador.js
+++ Proyecto/Cortiana Hermanos SRL/backend/routes/admin/administrador.js
@@ -1,13 +1,44 @@
 var express = require('express');
 var router = express.Router();
-
+var productosModel = require('../../models/productosModel');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('admin/administrador', {
-    layout: 'admin/layout',
-    user_admin: req.session.nombre
-  });
+router.get('/', async function (req, res, next) {
+    const productos = await productosModel.getProductos();
+    res.render('admin/administrador', {
+        layout: 'admin/layout',
+        user_admin: req.session.nombre,
+        productos
+    });
 });
+
+router.get('/nuevo',(req,res,next)=>{
+    res.render('admin/nuevo',{
+        layout: 'admin/layout'
+    });
+});
+
+
+router.post('/nuevo',async(req,res,next)=>{
+    try{
+        if(req.body.tipo != "" && req.body.clase != "" && req.body.descripcion != "" ){
+            await productosModel.insertProducto(req.body);
+            res.redirect('/admin/administrador')
+        }else{
+            res.render('admin/nuevo'),{
+                layout: 'admin/layout',
+                message:"Todos los campos son requeridos"
+            }
+        }
+    }catch(error){
+        console.log(error)
+        res.render('admin/nuevo'),{
+            layout: 'admin/layout',
+            error: true,
+            message: "No se cargo el producto"
+        }
+    }
+
+})
 
 module.exports = router;
