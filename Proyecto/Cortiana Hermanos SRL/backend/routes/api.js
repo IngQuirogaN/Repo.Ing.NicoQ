648b000162efbfc511212061578f409a5c623ca2
Nicolas Quiroga <nicolas_quiroga_90@hotmail.com>
2023-08-17 21:55:18 -0300

    Modulo 6 Unidad 4

--- Proyecto/Cortiana Hermanos SRL/backend/routes/api.js
+++ Proyecto/Cortiana Hermanos SRL/backend/routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var productosModel = require('./../models/productosModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
 router.get('/productos',async function (req,res,next){
     let productos = await productosModel.getProductos();
@@ -25,6 +26,34 @@ router.get('/productos',async function (req,res,next){
         }
     });
     res.json(productos);
+});
+
+
+
+router.post('/contacto',async(req,res)=>{
+    const mail = {
+        to: 'nicolas_quiroga_90@hotmail.com',
+        subject: 'Contacto web',
+        html: `${req.body.apellido} ${req.body.nombre} de ${req.body.empresa}  correo: ${req.body.email} telefono: ${req.body.telefono}<br>Mensaje: ${req.body.mensaje}`
+}
+
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth:{
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+    
+
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje enviado'
+    });
+
 });
 
 module.exports = router;
--- Proyecto/Cortiana Hermanos SRL/frontend/src/pages/Contacto.js
+++ Proyecto/Cortiana Hermanos SRL/frontend/src/pages/Contacto.js
@@ -1,65 +1,105 @@
+
+import React, { useState } from 'react';
+import axios from 'axios';
+
 import '../styles/Contacto.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faFacebook,faInstagram,faWhatsapp} from '@fortawesome/free-brands-svg-icons'
+import { faFacebook, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+
 
 const Contacto = (props) => {
 
-    return(
+    const initialForm = {
+        nombre: '',
+        apellido: '',
+        empresa: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
+    }
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true)
+        const response = await
+            axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm)
+        }
+    }
+
+    return (
         <main className="main_contacto">
-        <div className="formulario_contacto">
-            <div>
-                <h2>Complete el siguiente formulario:</h2>
-                <form action="" method="" class="formulario">
-                    <div>
-                        <label for="nombre">Nombre</label>
-                        <input type="text" name=""/>
-                    </div>
-                    
-                    <div>
-                        <label for="apellido">Apellido</label>
-                        <input type="text" name=""/>
-                    </div>
-                    <div>
-                        <label for="empresa">Empresa</label>
-                        <input type="text" name=""/>
-                    </div>
-                    <div>
-                        <label for="mail">Mail</label>
-                        <input type="text" name=""/>
-                    </div>
-                    <div>
-                        <label for="telefono">Telefono</label>
-                        <input type="text" name=""/>
-                    </div>
-                    <div class="text_area">
-                        <label for="mensaje">Mensaje</label>
-                        <textarea name="" id="" cols="30" rows="10"></textarea>
-                    </div>
-                    <div class="boton">
-                        <input type="submit" value="Enviar"/>
-                    </div>
-                </form>
+            <div className="formulario_contacto">
+                <div>
+                    <h2>Complete el siguiente formulario:</h2>
+                    <form action="" method="" className="formulario" onSubmit={handleSubmit}>
+                        <div>
+                            <label for="nombre">Nombre</label>
+                            <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} />
+                        </div>
+
+                        <div>
+                            <label for="apellido">Apellido</label>
+                            <input type="text" name="apellido" value={formData.apellido} onChange={handleChange} />
+                        </div>
+                        <div>
+                            <label for="empresa">Empresa</label>
+                            <input type="text" name="empresa" value={formData.empresa} onChange={handleChange} />
+                        </div>
+                        <div>
+                            <label for="mail">Mail</label>
+                            <input type="text" name="email" value={formData.email} onChange={handleChange} />
+                        </div>
+                        <div>
+                            <label for="telefono">Telefono</label>
+                            <input type="text" name="telefono" value={formData.telefono} onChange={handleChange} />
+                        </div>
+                        <div class="text_area">
+                            <label for="mensaje">Mensaje</label>
+                            <textarea name="mensaje" id="" cols="30" rows="10" value={formData.mensaje} onChange={handleChange}></textarea>
+                        </div>
+                        {sending ? <p>Enviando...</p> : null}
+                        {msg ? <p>{msg}</p> : null}
+                        <div class="boton">
+                            <input type="submit" value="Enviar" />
+                        </div>
+                    </form>
+                </div>
+                <div className="redes">
+                    <h2>Nuestras Redes Sociales</h2>
+                    <ul>
+                        <li><a href="https://www.facebook.com/cortianahnossrl"><FontAwesomeIcon icon={faFacebook} style={{ width: 40 + "px", scale: 200 + "%" }} /></a></li>
+                        <li><a href="https://www.instagram.com/cortianahnossrl/"><FontAwesomeIcon icon={faInstagram} style={{ width: 40 + "px", scale: 200 + "%" }} /></a></li>
+                        <li><a href="https://wa.me/541153773877"><FontAwesomeIcon icon={faWhatsapp} style={{ width: 40 + "px", scale: 200 + "%" }} /></a></li>
+
+                    </ul>
+                    <h2>Direccion</h2>
+                    <p> 9 de Julio & calle Nro. 17</p>
+                    <p> Villa del Rosario, Entre Rios, Argentina</p>
+                    <p>CP: 3229</p>
+                    <p>Telefono: 234-46822347</p>
+                    <p>info@cortianahns.com.ar </p>
+                    <p>Lunes – Viernes de 8:00 a 18:00 h. </p>
+                </div>
+
             </div>
-            <div className="redes">
-                <h2>Nuestras Redes Sociales</h2>
-                <ul>
-                    <li><a href="https://www.facebook.com/cortianahnossrl"><FontAwesomeIcon icon={faFacebook} style={{width: 40+"px",scale: 200+"%"}} /></a></li>
-                    <li><a href="https://www.instagram.com/cortianahnossrl/"><FontAwesomeIcon icon={faInstagram}style={{width: 40+"px",scale: 200+"%"}}/></a></li>
-                    <li><a href="https://wa.me/541153773877"><FontAwesomeIcon icon={faWhatsapp}style={{width: 40+"px",scale: 200+"%"}}/></a></li>
 
-                </ul>
-                <h2>Direccion</h2>
-                <p> 9 de Julio & calle Nro. 17</p>
-                <p> Villa del Rosario, Entre Rios, Argentina</p>
-                <p>CP: 3229</p>
-                <p>Telefono: 234-46822347</p>
-                <p>info@cortianahns.com.ar </p>
-                <p>Lunes – Viernes de 8:00 a 18:00 h. </p>
-            </div>
-
-        </div>
-
-    </main>
+        </main>
 
 
     );
